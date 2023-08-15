5c49bfeb04a174a3faff9e4540a79e54d3413bbc
Daphne271991 <134757720+Daphne271991@users.noreply.github.com>
2023-08-15 14:01:59 -0600

    main structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello World
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
