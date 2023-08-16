1e0e9a0250c3f2a5b63176a008b27f5a3a63d2f0
Daphne271991 <134757720+Daphne271991@users.noreply.github.com>
2023-08-15 19:02:52 -0600

    fixed API key for pexels

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,7 +24,7 @@ export default function Dictionary(props) {
     axios.get(apiUrl).then(handleDictionResponse);
 
     let pexelsApiKey =
-      "563492ad6f91700001000001fdd29f0808df42bd90c33f42e128fa89";
+      "BaaPRGnwxzaLrVr5XEPdyGSZfT92U2NWMueMHcqae4LBrPHGcmZzdIcV";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     let headers = { Authorization: `${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
